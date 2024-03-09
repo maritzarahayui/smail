0660fc63380df7a137de23a9e68de408fee928c2
Hana <hana@Hanas-MacBook-Pro.local>
2024-03-09 13:03:02 +0700

    done integ surat masuk, auth, navbar

--- smail/src/main/java/propensi/smail/controller/SuratMasukController.java
+++ smail/src/main/java/propensi/smail/controller/SuratMasukController.java
@@ -1,6 +1,7 @@
 package propensi.smail.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.Base64;
@@ -27,20 +28,36 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import propensi.smail.model.SuratMasuk;
+import propensi.smail.model.user.Pengguna;
+import propensi.smail.repository.PenggunaDb;
+import propensi.smail.service.PenggunaService;
 import propensi.smail.service.SuratMasukService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
 @Controller
-@RequestMapping("/api/surat-masuk")
+@RequestMapping("/surat-masuk")
 public class SuratMasukController {
 
     @Autowired
     private SuratMasukService suratMasukService;
 
+    @Autowired
+    PenggunaDb penggunaDb;
+
+    @Autowired
+    PenggunaService penggunaService;
+
     @PostMapping("/upload")
-    public String uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("kategori") String kategori, @RequestParam("perihal") String perihal, @RequestParam("tanggalDibuat") String tanggalDibuat, @RequestParam("status") int status, @RequestParam("pengirim") String pengirim, @RequestParam("tembusan") String tembusan) throws ParseException {
+    public String uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("kategori") String kategori, 
+        @RequestParam("perihal") String perihal, @RequestParam("tanggalDibuat") String tanggalDibuat, 
+        @RequestParam("status") int status, @RequestParam("pengirim") String pengirim, 
+        @RequestParam("tembusan") String tembusan, Authentication auth) throws ParseException {
+
         String message = "";
         //convert type of tanggalDibuat from String to Date
         SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
@@ -52,17 +69,17 @@ public class SuratMasukController {
 
             message = "Uploaded the file successfully: " + file.getOriginalFilename();
             // redirect to detail surat masuk
-            return "redirect:/api/surat-masuk/detail/" + suratMasuk.getNomorArsip();
+            return "redirect:/surat-masuk/detail/" + suratMasuk.getNomorArsip();
         }catch (Exception e) {
             message = "Could not upload the file: " + file.getOriginalFilename() + "!";
-            return "redirect:/api/surat-masuk/form";
+            return "redirect:/surat-masuk/form";
         }
     }
 
     @GetMapping("/files")
     public ResponseEntity<List<String>> getListFiles() {
         List<String> fileNames = suratMasukService.getAllFiles().map(fileName -> {
-            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/surat-masuk/files/").path(fileName.getNomorArsip()).toUriString();
+            String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/surat-masuk/files/").path(fileName.getNomorArsip()).toUriString();
             return fileDownloadUri;
         }).collect(Collectors.toList());
 
@@ -70,7 +87,7 @@ public class SuratMasukController {
     }
 
     @GetMapping("/download/{id}")
-    public ResponseEntity<byte[]> downloadFile(@PathVariable String id) {
+    public ResponseEntity<byte[]> downloadFile(@PathVariable String id, Authentication auth) {
         SuratMasuk file = suratMasukService.getFile(id);
 
         if (file != null) {
@@ -89,16 +106,32 @@ public class SuratMasukController {
 
     //get all surat masuk
     @GetMapping("/all")
-    public String getAllSuratMasuk(Model model) {
+    public String getAllSuratMasuk(Model model, Authentication auth) {
         List<SuratMasuk> suratMasukList = suratMasukService.getAllSuratMasuk();
+
         //masukan ke thymeleaf
         model.addAttribute("suratMasukList", suratMasukList);
+
+        if (auth != null) {
+            OidcUser oauthUser = (OidcUser) auth.getPrincipal();
+            String email = oauthUser.getEmail();
+            Optional<Pengguna> user = penggunaDb.findByEmail(email);
+
+            if (user.isPresent()) {
+                Pengguna pengguna = user.get();
+                model.addAttribute("role", penggunaService.getRole(pengguna));
+                model.addAttribute("namaDepan", penggunaService.getFirstName(pengguna));
+            } else {
+                return "auth-failed";
+            }
+        }
+
         return "daftar-arsip-tes";
     }
 
     // Metode untuk menampilkan preview PDF
     @GetMapping("/detail/{id}")
-    public String previewPDF(@PathVariable("id") String id, Model model) throws IOException {
+    public String previewPDF(@PathVariable("id") String id, Model model, Authentication auth) throws IOException {
         SuratMasuk suratMasuk = suratMasukService.getFile(id);
         byte[] pdf = suratMasuk.getFile();
 
@@ -108,6 +141,21 @@ public class SuratMasukController {
         model.addAttribute("base64PDF", base64PDF);
         model.addAttribute("suratMasuk", suratMasuk);
         model.addAttribute("statusText", getStatusText(suratMasuk.getStatus()));
+
+        if (auth != null) {
+            OidcUser oauthUser = (OidcUser) auth.getPrincipal();
+            String email = oauthUser.getEmail();
+            Optional<Pengguna> user = penggunaDb.findByEmail(email);
+
+            if (user.isPresent()) {
+                Pengguna pengguna = user.get();
+                model.addAttribute("role", penggunaService.getRole(pengguna));
+                model.addAttribute("namaDepan", penggunaService.getFirstName(pengguna));
+            } else {
+                return "auth-failed";
+            }
+        }
+
         return "detail-arsip-tes"; 
     }
 
@@ -127,24 +175,39 @@ public class SuratMasukController {
     
     // route to form-upload-surat
     @GetMapping("/form")
-    public String formUploadSurat(Model model) {
+    public String formUploadSurat(Model model, Authentication auth) {
+
+        if (auth != null) {
+            OidcUser oauthUser = (OidcUser) auth.getPrincipal();
+            String email = oauthUser.getEmail();
+            Optional<Pengguna> user = penggunaDb.findByEmail(email);
+
+            if (user.isPresent()) {
+                Pengguna pengguna = user.get();
+                model.addAttribute("role", penggunaService.getRole(pengguna));
+                model.addAttribute("namaDepan", penggunaService.getFirstName(pengguna));
+            } else {
+                return "auth-failed";
+            }
+        }
+
         return "form-arsip-tes";
     }
 
     @GetMapping("/form-arsip")
-    public String form(Model model) {
+    public String form(Model model, Authentication auth) {
         return "form-surat-masuk";
     }
 
     // route to semua-surat-masuk
     @GetMapping("/daftar-arsip")
-    public String semuaSuratMasuk(Model model) {
+    public String semuaSuratMasuk(Model model, Authentication auth) {
         return "daftar-arsip-tes";
     }
 
     // route to detail-surat-masuk
     @GetMapping("/detail-surat-masuk")
-    public String detailSuratMasuk(Model model) {
+    public String detailSuratMasuk(Model model, Authentication auth) {
         return "detail-surat-masuk";
     }
     
--- smail/src/main/java/propensi/smail/security/WebSecurityConfig.java
+++ smail/src/main/java/propensi/smail/security/WebSecurityConfig.java
@@ -12,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
@@ -49,10 +50,14 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/").permitAll()
                     .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                     .requestMatchers("/profile").hasAnyAuthority("ADMIN", "STAF", "DOSEN", "MAHASISWA", "PENGURUS")
+                    .requestMatchers("/surat-masuk/all", "/surat-masuk/detail/*", "/surat-masuk/download/*").hasAnyAuthority("PENGURUS")
+                    .requestMatchers("/surat-masuk/**").hasAnyAuthority("ADMIN")
+                    .requestMatchers(HttpMethod.POST, "/surat-masuk/upload").hasAnyAuthority("ADMIN") // Izinkan akses POST untuk testing
                     // .requestMatchers("/staf").hasAnyAuthority("STAF")
                     // .requestMatchers("/admin").hasAnyAuthority("ADMIN")
                     .anyRequest().authenticated())
